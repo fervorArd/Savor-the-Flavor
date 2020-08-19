861f7dd6e7a0e0c51eeee33de5fd2b2f9fc2581b
BhavnaSoni853 <bhavnasoni8530@gmail.com>
2020-08-19 15:03:57 +0530

    Admin Login Authentication

--- config/auth.js
+++ config/auth.js
@@ -16,5 +16,14 @@ module.exports = {
       req.flash('error_msg', 'Please log in to view this resources');
       res.redirect('/user/login');
     }
+  },
+  adminLogin: function(req, res, next){
+    if(req.isAuthenticated() && req.user.isAdmin){
+      return next();
+    }else{
+      req.session.oldUrl = `/admin${req.url}`;
+      req.flash('error_msg', 'Please log in to view this resources');
+      res.redirect('/user/login');
+    }
   }
 }
